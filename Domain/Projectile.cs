68349236c8f355c842af5dfc40aa9f8c208b5101
FirstSlip <86049908+FirstSlip@users.noreply.github.com>
2023-01-11 16:29:34 +0500

    Upload Prohect

---
+++ Domain/Projectile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Domain
+{
+    class Projectile : GameObject
+    {
+        private Point direction;
+        private double distance;
+        private int speed;
+        public int damage = 30;
+        public int Speed { get => speed; }
+        public Projectile(PictureBox image, Bitmap imageFile, Point startLocation, Point direction, double distance, int speed = 20)
+        {
+            this.speed = speed;
+            this.image = image;
+            this.direction = direction;
+            this.distance = distance;
+            SetPictureBoxSettings(imageFile, startLocation);
+        }
+
+        private void SetPictureBoxSettings(Bitmap imageFile, Point location)
+        {
+            image.BackColor = System.Drawing.Color.Transparent;
+            image.Image = imageFile;
+            image.Location = location;
+            image.Name = "projectile";
+            image.Size = new System.Drawing.Size(50, 50);
+            image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            image.TabIndex = 0;
+            image.TabStop = false;
+        }
+
+        public void Move()
+        {
+            Location = new Point(Location.X + (int)(direction.X / distance * Speed), Location.Y + (int)(direction.Y / distance * Speed));
+        }
+    }
+}
